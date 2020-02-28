24e29030271dd29f7f416401b6986ad7362670d2
superPershing <727342027@qq.com>
2020-02-28 21:26:38 +0800

    [refactor](*): change member variable access modifier to private

--- src/main/java/com/gildedrose/Item.java
+++ src/main/java/com/gildedrose/Item.java
@@ -2,15 +2,35 @@ package com.gildedrose;
 
 public class Item {
 
-    public String name;
+    private String name;
 
-    public int sell_in;
+    private int sell_in;
 
-    public int quality;
+    private int quality;
 
     public Item(String name, int sell_in, int quality) {
         this.name = name;
         this.sell_in = sell_in;
+        this.quality = quality;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getSell_in() {
+        return sell_in;
+    }
+
+    public void setSell_in(int sell_in) {
+        this.sell_in = sell_in;
+    }
+
+    public int getQuality() {
+        return quality;
+    }
+
+    public void setQuality(int quality) {
         this.quality = quality;
     }
 
--- src/test/java/com/gildedrose/GildedRoseTest.java
+++ src/test/java/com/gildedrose/GildedRoseTest.java
@@ -1,20 +1,21 @@
 package com.gildedrose;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.*;
-
 import org.junit.Test;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class GildedRoseTest {
 
     @Test
     public void foo() {
-        Item[] items = new Item[] { new Item("foo", 1, 5) };
+        Item[] items = new Item[]{new Item("foo", 1, 5)};
         GildedRose app = new GildedRose(items);
         app.update_quality();
-        assertEquals("foo", app.items[0].name);
-        assertThat(app.items[0].quality, is(4));
-        assertThat(app.items[0].sell_in, is(0));
+        assertEquals("foo", app.getItems()[0].getName());
+        assertThat(app.getItems()[0].getQuality(), is(4));
+        assertThat(app.getItems()[0].getSell_in(), is(0));
     }
 
 }
