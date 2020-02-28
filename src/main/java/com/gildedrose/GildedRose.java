db7cbbf47edaafa6270123bae7a61a67ed96a88e
superPershing <727342027@qq.com>
2020-02-28 23:07:23 +0800

    [refactor](GildedRose): remove for cycle and extract updateSingleItemQuality method

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -1,5 +1,7 @@
 package com.gildedrose;
 
+import java.util.Arrays;
+
 public class GildedRose {
     private static final String AGED_BRIE = "Aged Brie";
     private static final String BACKSTAGE = "Backstage passes to a TAFKAL80ETC concert";
@@ -12,40 +14,42 @@ public class GildedRose {
     }
 
     public void updateQuality() {
-        for (Item item : items) {
-            if (!item.isNameEqualTo(AGED_BRIE) && !item.isNameEqualTo(BACKSTAGE)
-                    && item.getQuality() > 0 && !item.isNameEqualTo(SULFURAS)) {
-                item.setQuality(item.getQuality() - 1);
-            } else if (item.getQuality() < 50) {
-                item.setQuality(item.getQuality() + 1);
+        Arrays.stream(items).forEach(this::updateSingleItemQuality);
+    }
 
-                if (item.isNameEqualTo(BACKSTAGE)) {
-                    if (item.getSellIn() < 11 && item.getQuality() < 50) {
-                        item.setQuality(item.getQuality() + 1);
-                    }
+    private void updateSingleItemQuality(Item item) {
+        if (!item.isNameEqualTo(AGED_BRIE) && !item.isNameEqualTo(BACKSTAGE)
+                && item.getQuality() > 0 && !item.isNameEqualTo(SULFURAS)) {
+            item.setQuality(item.getQuality() - 1);
+        } else if (item.getQuality() < 50) {
+            item.setQuality(item.getQuality() + 1);
 
-                    if (item.getSellIn() < 6 && item.getQuality() < 50) {
-                        item.setQuality(item.getQuality() + 1);
-                    }
-                }
-            }
-
-            if (!item.isNameEqualTo(SULFURAS)) {
-                item.setSellIn(item.getSellIn() - 1);
-            }
-
-            if (item.getSellIn() < 0) {
-                if (!item.isNameEqualTo(AGED_BRIE)) {
-                    if (!item.isNameEqualTo(BACKSTAGE)) {
-                        if (item.getQuality() > 0 && !item.isNameEqualTo(SULFURAS)) {
-                            item.setQuality(item.getQuality() - 1);
-                        }
-                    } else {
-                        item.setQuality(0);
-                    }
-                } else if (item.getQuality() < 50) {
+            if (item.isNameEqualTo(BACKSTAGE)) {
+                if (item.getSellIn() < 11 && item.getQuality() < 50) {
                     item.setQuality(item.getQuality() + 1);
                 }
+
+                if (item.getSellIn() < 6 && item.getQuality() < 50) {
+                    item.setQuality(item.getQuality() + 1);
+                }
+            }
+        }
+
+        if (!item.isNameEqualTo(SULFURAS)) {
+            item.setSellIn(item.getSellIn() - 1);
+        }
+
+        if (item.getSellIn() < 0) {
+            if (!item.isNameEqualTo(AGED_BRIE)) {
+                if (!item.isNameEqualTo(BACKSTAGE)) {
+                    if (item.getQuality() > 0 && !item.isNameEqualTo(SULFURAS)) {
+                        item.setQuality(item.getQuality() - 1);
+                    }
+                } else {
+                    item.setQuality(0);
+                }
+            } else if (item.getQuality() < 50) {
+                item.setQuality(item.getQuality() + 1);
             }
         }
     }
